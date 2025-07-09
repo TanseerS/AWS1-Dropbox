f6fae09006e921f517b06ab4cfea0f4a51404446
Tanseer <tanseer@skillminds.in>
2025-07-09 10:27:30 +0530

    s3 bucket outputs

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -9,3 +9,15 @@ output "bucket-id" {
   description = "The ID of the S3 bucket"
   value       = aws_s3_bucket.dropbox-bucket.id
 }
+
+# Bucket ARN
+output "bucket-arn" {
+  description = "The ARN of the S3 bucket"
+  value       = aws_s3_bucket.dropbox-bucket.arn
+}
+
+# Bucket Domain Name
+output "bucket-domain-name" {
+  description = "The domain name of the S3 bucket"
+  value       = aws_s3_bucket.dropbox-bucket.bucket_domain_name
+}
